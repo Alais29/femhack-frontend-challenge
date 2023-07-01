a357d338ef58bf41d0348fc928ff8a4e2d2c1447
Alfonsina <alfonsina.l@gmail.com>
2023-07-01 14:22:43 -0400

    feat: add bar chart for users by year data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { UsersYear } from './components/UsersYear'
 
 export const App = () => {
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>It works and you found me!</h1>
-      <h1>Vite + React BoilerPlate ✨</h1>
+      <UsersYear />
     </>
   )
 }
---
+++ src/hooks/useUsersByYear.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { getUsersXYear } from '../../services/getUsersXYear';
+
+/**
+ * Takes an array of years as input and returns a state variable `data` that holds 
+ * the response data from an API call for each year.
+ * @returns The `data` state variable.
+ */
+export const useUsersByYear = (years) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const ApiCallPromise = (year) => {
+      return new Promise((resolve, reject) => {
+        getUsersXYear(year)
+          .then(response => { resolve(response) })
+          .catch(error => { reject(error) })
+      });
+    };
+
+    const promises = years.map(year => ApiCallPromise(year));
+
+    Promise.all(promises)
+      .then(response => {
+        console.log(response);
+        setData(response);
+      })
+      .catch(error => {
+        console.log('There was an error:', error);
+      });
+  }, [years]);
+
+  return data;
+};
